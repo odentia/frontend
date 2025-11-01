d8d6af4535a0916e693f86fb0575bac02a9f4b39
Zertonixa <olpin9070@gmail.com>
2025-11-01 21:24:44 +0300

    chore(lint): linters

--- packages/ui/src/input/input.tsx
+++ packages/ui/src/input/input.tsx
@@ -2,6 +2,6 @@ interface InputProps {
   onValueChange: () => void;
 }
 
-export const Input = ({onValueChange}: InputProps) => {
-  onValueChange()
+export const Input = ({ onValueChange }: InputProps) => {
+  onValueChange();
 };
