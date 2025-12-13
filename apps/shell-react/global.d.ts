3c50bc41f8bcc90bd61c3355038ecf22f7f975e1
Zertonixa <olpin9070@gmail.com>
2025-12-13 20:22:13 +0300

    chore(config): update config

--- apps/shell-react/global.d.ts
+++ apps/shell-react/global.d.ts
@@ -14,3 +14,10 @@ declare module "*.css" {
   const content: { [className: string]: string };
   export default content;
 }
+
+declare module "*.svg" {
+  import type { FC, SVGProps } from "react";
+
+  const Component: FC<SVGProps<SVGSVGElement>>;
+  export default Component;
+}
