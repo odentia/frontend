981a5886735d89ed07ca61e6a6c5295053b22dbe
Zertonixa <olpin9070@gmail.com>
2025-12-28 17:56:55 +0300

    fix(home): fix home styles

--- apps/me-profile/src/app/pages/Home.tsx
+++ apps/me-profile/src/app/pages/Home.tsx
@@ -5,7 +5,9 @@ import styles from "./home.module.scss";
 export const Home = () => {
   return (
     <div className={styles.container}>
-      <ThemeRedactor />
+      <div className={styles.containerTheme}>
+        <ThemeRedactor />
+      </div>
       <Profile />
     </div>
   );
